913a70f5f3ec5aa952aeb8ab6cb0cb23c42e3830
Mohammed Agboola®️ <moh.agboola@gmail.com>
2021-11-04 02:42:41 +0100

    behavioral pattern - observer

---
+++ observer-pattern.js
@@ -0,0 +1,34 @@
+class Publisher {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn);
+  }
+
+  unsubscribe(fnToRemove) {
+    this.observers = this.observers.filter(fn => {
+      if (fn !== fnToRemove) return fn;
+    });
+  }
+
+  trigger() {
+    this.observers.forEach(fn => fn.call());
+  }
+}
+
+function observer1() {
+  console.log('obserser1 triggered');
+}
+
+function observer2() {
+  console.log('obserser2 triggered');
+}
+
+const publisher = new Publisher();
+publisher.subscribe(observer1);
+publisher.subscribe(observer2);
+publisher.trigger();
+publisher.unsubscribe(observer2);
+publisher.trigger();
